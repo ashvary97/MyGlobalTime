d681385620e2e2d151ac4dc7a9d402ee7c835333
ashvary97 <55987205+ashvary97@users.noreply.github.com>
2025-08-25 01:49:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const headerPlaceholder = document.getElementById('main-header');
+    
+    if (headerPlaceholder) {
+        fetch('header.html')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.text();
+            })
+            .then(data => {
+                headerPlaceholder.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+                headerPlaceholder.innerHTML = '<p>Error loading header. Please try again later.</p>';
+            });
+    }
+});
